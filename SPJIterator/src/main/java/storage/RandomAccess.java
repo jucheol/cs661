2d6725b73a8c5b96edcf288a53e6fa42bae76162
Jucheol Moon <jucheol.moon@gmail.com>
2016-03-23 13:00:22 -0500

    obsolete main

--- SPJIterator/src/main/java/storage/RandomAccess.java
+++ SPJIterator/src/main/java/storage/RandomAccess.java
@@ -110,4 +110,48 @@ public class RandomAccess {
 		System.out.println('\n' + "Request for deallocation of page " + pageNumber
 				+ " received");
 	}
+	
+	public static void main(String args[]) {
+
+		/**
+		 * Buffer to write the data to the file
+		 */
+		byte[] writeBuffer = new byte[1024];
+		writeBuffer = "HelloWorld".getBytes();
+
+		/**
+		 * Location of the file to which the read/write operations are performed
+		 */
+		String path = "data/device_0.RAF";
+
+		/**
+		 * Buffer to read the data from the file
+		 */
+		byte[] readBuffer = new byte[1024];
+
+		int pageNumber = -1;
+
+		RandomAccess randomAccess = new RandomAccess(path);
+
+		pageNumber = randomAccess.allocatePage();
+		
+		if (pageNumber < 0) {
+			System.out.println("Page allocation failed");
+		} else {
+			System.out.println("Page successfully allocated");
+		}
+
+		randomAccess.writePage(writeBuffer, pageNumber);
+
+		randomAccess.readPage(readBuffer, pageNumber);
+
+		/**
+		 * prints the data read from the file
+		 */
+		for (int i = 0; i < readBuffer.length; i++) {
+			System.out.print(readBuffer[i]);
+		}
+		
+		randomAccess.deallocatePage(pageNumber);
+	}
 }
