4f996a8a1143075b15d708b751a24c147ceef8c4
daolinISU <dlcheng@iastate.edu>
2016-04-24 22:33:25 -0500

    Iterator with SP Iteratore done

---
+++ SPJIterator/src/main/java/iterator/SPIterator.java
@@ -0,0 +1,15 @@
+package iterator;
+
+import database.Tuple;
+
+public class SPIterator {
+	public boolean hasNext() {
+		return false;
+	}
+	public Tuple getNext() {
+		return null;
+	}
+	public void rewind() {
+	}
+	
+}
